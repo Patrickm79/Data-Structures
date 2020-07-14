1bc2729464a4ac38721682bd0de114b89f6d87c2
Patrick Millet <lohs79seahawk@gmail.com>
2020-07-14 17:45:09 -0500

    Day 2 - Stack / Queue complete

--- stack/stack.py
+++ stack/stack.py
@@ -11,17 +11,42 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
-from singly_linked_list import singly_linked_list
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def pop(self):
+#         if self.size == 0:
+#             return 
+#         else:
+#             self.size -= 1
+#             return self.storage.pop(-1)
+
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.insert_at_head(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return 
+        else:
+            self.size -= 1
+            return self.storage.remove_head()
